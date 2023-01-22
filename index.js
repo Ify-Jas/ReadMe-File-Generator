fb38659554fa85a2987bc4efbf6034f1c6d1f01e
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-01-22 23:43:08 +0000

    updating my prompt file

--- index.js
+++ index.js
@@ -2,29 +2,18 @@ const fs = require("fs");
 const path = require('path');
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-const questionPrompt = require('./lib/questions.js');
-const prompt = inquirer.createPromptModule();
+const initiateQuestions  = require("./lib/questions.js");
 
 
 
-// function to write README file
-function createFile() {
-    .then(data => {
-        fs.writeFile(data.fileName +'.md', data.content, (err)=> {
-            if(err) return console.log(err);
-        
-            console.log(`you have successfully created the ${data.fileName} file`);
-        })
-    
-    })
-
-}
 
 // function to initialize program
 function init() {
-    createFile();
-    
+    initiateQuestions.initiateQuestions();
+       
 }
 
 // function call to initialize program
 init();
+
+
